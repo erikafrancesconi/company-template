8d580505616bd6e23b94ce8cfb25b54a533c94b2
Erika Francesconi <fiorediren@gmail.com>
2020-10-20 20:48:20 +0200

    Responsive Dropdown Navigation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-const hamb = document.querySelector('.hamburger');
+(function() {
+  // Hamburger Menu
+  const hamb = document.querySelector('.hamburger');
 
-const classToggle = () => {
-  const navs = document.querySelectorAll('.hamburger-noshow');
-  navs.forEach(nav => nav.classList.toggle('hamburger-show'));
-  hamb.setAttribute('aria-expanded', hamb.getAttribute('aria-expanded') === 'true' ? 'false' : true);
-}
-
-hamb.addEventListener('click', classToggle);
-hamb.addEventListener('keyup', event => {
-  if (event.keyCode == 13 || event.keyCode == 32) {
-    classToggle();
+  const classToggle = () => {
+    const navs = document.querySelectorAll('.hamburger-noshow');
+    navs.forEach(nav => nav.classList.toggle('hamburger-show'));
+    hamb.setAttribute('aria-expanded', hamb.getAttribute('aria-expanded') === 'true' ? 'false' : true);
   }
-})
+
+  hamb.addEventListener('click', classToggle);
+  hamb.addEventListener('keyup', event => {
+    if (event.keyCode == 13 || event.keyCode == 32) {
+      classToggle();
+    }
+  })
+
+  // Dropdown Navigation
+  // const menuItems = document.querySelectorAll('li.dropdown');
+  // let timer;
+  // Array.prototype.forEach.call(menuItems, function(el) {
+  //   el.querySelector('a').addEventListener("click",  function(event) {
+  //     event.preventDefault();
+  //     if (this.parentNode.className == "dropdown") {
+  //       this.parentNode.className = "dropdown open";
+  //       this.setAttribute('aria-expanded', "true");
+  //     } else {
+  //       this.parentNode.className = "dropdown";
+  //       this.setAttribute('aria-expanded', "false");
+  //     }
+  //     return false;
+  //   });
+  // });
+})();
